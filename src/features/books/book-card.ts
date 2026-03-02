4164f8ccf5f3b20adad9a5b6be6ddefe3abb208c
GOLICK-PC\Golic <Golick@tut.by>
2026-03-02 18:53:37 +0300

    Added raitng directive

--- src/features/books/book-card.ts
+++ src/features/books/book-card.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Book } from '../../core/books';
+import { AppIfHighRating } from '../../shared/if-high-rating';
 
 @Component({
   selector: 'book-card',
   templateUrl: './book-card.html',
-  styleUrl: './book-card.less'
+  styleUrl: './book-card.less',
+  imports: [AppIfHighRating]
 })
 export class BookCardComponent {
 
---
+++ src/shared/if-high-rating.ts
@@ -0,0 +1,26 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from "@angular/core";
+
+@Directive(
+    {
+        selector: '[appIfHighRating]',
+        standalone: true,
+    }
+)
+export class AppIfHighRating{
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef)
+    {}
+
+    @Input() set appIfHighRating(rating: number)
+    {
+        if (rating > 4.3)
+        {
+            this.viewContainer.createEmbeddedView(this.templateRef);
+        } 
+        else
+        {
+            this.viewContainer.clear();
+        }
+    }
+}
